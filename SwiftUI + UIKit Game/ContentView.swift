4991fae8afb5ec08908de6015c061af177581af4
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-06-12 08:29:37 +0300

    Initial Commit

---
+++ SwiftUI + UIKit Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI + UIKit Game
+//
+//  Created by Mikhail Bukhrashvili on 12.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
